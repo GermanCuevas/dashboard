77cf3e9f6857b7259c6f4e2bed2265937ba76e77
German Cuevas <german.1990.cuevas@gmail.com>
2023-05-28 17:03:23 -0300

    feat:componentes varios, plantilla

--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -1,4 +1,26 @@
+import './formLogin.css';
+import { useNavigate } from 'react-router-dom';
 const FormLogin = () => {
-  return <h1>FormLogin</h1>;
+  const navigate = useNavigate();
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate('/dashboard');
+  }
+
+  return (
+    <div className="boxForm">
+      <form onSubmit={handleSubmit}>
+        <div className="boxInput">
+          <label htmlFor="name">Nombre:</label>
+          <input type="text" id="name" />
+        </div>
+        <div className="boxInput">
+          <label htmlFor="pass">Contraseña:</label>
+          <input type="text" id="pass" />
+        </div>
+        <button className="btnForm">Ingresar</button>
+      </form>
+    </div>
+  );
 };
 export default FormLogin;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,30 @@
+//import { useEffect, useState } from 'react';
+import './header.css';
+
 const Header = () => {
-  return <h2>Header</h2>;
+  /*   let [persona, setPersona] = useState(undefined);
+  useEffect(() => {
+    const myFetch = async () => {
+      const response = await fetch('/data.json');
+      const data = await response.json();
+      console.log(data);
+      setPersona(data[0]);
+    };
+    myFetch();
+  }, []);
+  console.log(persona); */
+
+  return (
+    <header>
+      <div className="navigation">
+        <ul>
+          <li className="li-name">
+            Hola
+            <img className="miImagen" src={'/public/avatar.avif'}></img>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 export default Header;
