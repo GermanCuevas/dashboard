32bb6deaf7eb2cbf5ad50d4bc5ad9f1949eea799
José Núñez <jolenuav@gmail.com>
2023-06-08 09:08:11 -0300

    'Se obtiene datos del clima con la api del back

--- src/components/Cards/Team1/Team1.jsx
+++ src/components/Cards/Team1/Team1.jsx
@@ -1,20 +1,30 @@
 import { useEffect, useState } from 'react';
 
 export const Team1 = () => {
-  const [respuesta, setRespuesa] = useState(undefined);
+  const [temperature, setTemperature] = useState(undefined);
   useEffect(() => {
     const llamada = async () => {
-      let response = await fetch('http://localhost:8080/ap1/saludos');
-      let data = await response.text();
-      setRespuesa(data);
+      // TODO se puede mejorar este fragmento de código
+      // se puede dejar la construcción del request del lado del back por ejemplo
+      const requestAPI = {
+        location: "-34.607319,-58.435705",
+        fields: 'temperature',
+        timesteps: 'best',
+        units: 'metric'
+      }
+      const response = await fetch('http://localhost:8080/weather?' + new URLSearchParams(requestAPI));
+      const responseData = await response.json();
+      const temperature = responseData[0].intervals[0].values.temperature;
+      setTemperature(temperature);
     };
     llamada();
   }, []);
 
   return (
     <div className="card">
-      <h1>Team1</h1>
-      <p>{respuesta}</p>
+      <h1>Team1 CLima</h1>
+      <p>{`${temperature} C`}</p>
+      <small>Buenos Aires</small>
     </div>
   );
 };
