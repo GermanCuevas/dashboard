11c66262d5b62c760b92dd9de896d46e0bebfb57
José Núñez <jolenuav@gmail.com>
2023-06-08 16:16:08 -0300

    Grupo1: Refactor Se cambia el servicio para poder buscar por ciudad o país

--- src/components/Cards/Team1/Team1.jsx
+++ src/components/Cards/Team1/Team1.jsx
@@ -1,30 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export const Team1 = () => {
   const [temperature, setTemperature] = useState(undefined);
+  const [country, setCountry] = useState(undefined);
+  const getWeather = async (request) => {
+    const response = await fetch(
+      "http://localhost:8081/realtime?" + new URLSearchParams(request)
+    );
+    const responseData = await response.json();
+    const temperature = responseData.temperature;
+    return temperature;
+  };
   useEffect(() => {
-    const llamada = async () => {
-      // TODO se puede mejorar este fragmento de código
-      // se puede dejar la construcción del request del lado del back por ejemplo
-      const requestAPI = {
-        location: "-34.607319,-58.435705",
-        fields: 'temperature',
-        timesteps: 'best',
-        units: 'metric'
-      }
-      const response = await fetch('http://localhost:8080/weather?' + new URLSearchParams(requestAPI));
-      const responseData = await response.json();
-      const temperature = responseData[0].intervals[0].values.temperature;
+    const countryMain = 'Buenos Aires';
+    const requestAPI = {
+      location: countryMain,
+      units: "metric",
+    };
+    const callApi = async () => {
+      const temperature = await getWeather(requestAPI);
       setTemperature(temperature);
     };
-    llamada();
+    callApi();
+    setCountry(countryMain);
   }, []);
 
   return (
     <div className="card">
       <h1>Team1 CLima</h1>
       <p>{`${temperature} C`}</p>
-      <small>Buenos Aires</small>
+      <small>{country}</small>
     </div>
   );
 };
