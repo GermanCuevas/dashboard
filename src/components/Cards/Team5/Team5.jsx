2d213d7636e159e93f881ae08b25fdcdf56bc2a2
Nico Uema <nicouemacapdevila@gmail.com>
2023-06-12 00:53:03 -0300

    Update Team5.jsx

--- src/components/Cards/Team5/Team5.jsx
+++ src/components/Cards/Team5/Team5.jsx
@@ -1,20 +1,32 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Team5 = () => {
-  const [respuesta, setRespuesa] = useState(undefined);
+  const [respuesta, setRespuesta] = useState(undefined);
+
   useEffect(() => {
     const llamada = async () => {
-      let response = await fetch('http://localhost:8080/api-5/greeting');
-      let data = await response.text();
-      setRespuesa(data);
+      try {
+        const response = await fetch('http://localhost:8085/crypto/crypto-prices?ids=bitcoin');
+        if (response.ok) {
+          const data = await response.json();
+          setRespuesta(data);
+        } else {
+          console.log('Failed to retrieve cryptocurrency prices');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
     };
+
     llamada();
   }, []);
+
+  const bitcoinPrice = respuesta && respuesta.bitcoin ? respuesta.bitcoin.usd : 0;
 
   return (
     <div className="card">
       <h1>Team5</h1>
-      <p>{respuesta}</p>
+      <p>Bitcoin: ${bitcoinPrice}</p>
     </div>
   );
 };
