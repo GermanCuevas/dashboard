d22bc47244255b9b29d739a930cccac08789f99c
German Cuevas <german.1990.cuevas@gmail.com>
2023-06-12 11:17:04 -0300

    feat: se agregan Modales

--- src/components/Cards/Team4/Team4.jsx
+++ src/components/Cards/Team4/Team4.jsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button, Modal } from 'react-bootstrap';
+import { faSliders } from '@fortawesome/free-solid-svg-icons';
 
 export const Team4 = () => {
   const [respuesta, setRespuesa] = useState(undefined);
+  const [showSettingModal, setShowSettingModal] = useState(false);
+  const handleShowSettingModal = () => setShowSettingModal(true);
+  const handleCloseSettingModal = () => setShowSettingModal(false);
+
   useEffect(() => {
     const llamada = async () => {
       let response = await fetch('http://localhost:8080/api/greeting');
@@ -12,9 +19,48 @@ export const Team4 = () => {
   }, []);
 
   return (
-    <div className="card">
-      <h1>Team4</h1>
-      <p>{respuesta}</p>
-    </div>
+    <>
+      <div className="card">
+        <FontAwesomeIcon
+          className="fw-bold fs-5 position-absolute"
+          style={{ right: '2rem', cursor: 'pointer' }}
+          icon={faSliders}
+          onClick={handleShowSettingModal}
+        ></FontAwesomeIcon>
+
+        <p>{respuesta}</p>
+      </div>
+
+      {/* MODAL EDITABLE PARA CADA TEAM */}
+      <Modal show={showSettingModal} onHide={handleCloseSettingModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Configuracion</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* Aqui van la interfaz front dependiendo de la funcionalidad, en caso de que necesiten ayuda con esta parte porfavor avisarme (German).*/}
+          <form className="my-form">
+            <div className="boxInput">
+              <label htmlFor="campo1">Campo 1</label>
+              <input type="text" id="campo1" />
+            </div>
+            <div className="boxInput">
+              <label htmlFor="campo2">Campo 2</label>
+              <input type="text" id="campo2" />
+            </div>
+            <div className="boxInput">
+              <label htmlFor="campo3">Campo 3</label>
+              <input type="text" id="campo3" />
+            </div>
+            <button>Enviar</button>
+          </form>
+          {/* Este form es solo para ejemplificar, como mencione, se puede cambiar dependediendo del proyecto */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseSettingModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
--- src/components/Cards/Team5/Team5.jsx
+++ src/components/Cards/Team5/Team5.jsx
@@ -1,13 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import { faSliders } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Team5 = () => {
+  const [showSettingModal, setShowSettingModal] = useState(false);
+  const handleShowSettingModal = () => setShowSettingModal(true);
+  const handleCloseSettingModal = () => setShowSettingModal(false);
   const [respuesta, setRespuesta] = useState(undefined);
   const [dolar, setDolar] = useState(undefined);
 
   useEffect(() => {
     const llamada = async () => {
       try {
-        const response = await fetch('http://localhost:8085/crypto/crypto-prices?ids=bitcoin');        
+        const response = await fetch(
+          'http://localhost:8085/crypto/crypto-prices?ids=bitcoin'
+        );
         if (response.ok) {
           const data = await response.json();
           setRespuesta(data);
@@ -24,22 +32,74 @@ export const Team5 = () => {
       } catch (error) {
         console.error('Error:', error);
       }
-      console.log("RESULTADO " + data);
+      //Esta linea no se esta ejecutando===>
+      //console.log('RESULTADO ' + data);
     };
 
     llamada();
   }, []);
 
-  const bitcoinPrice = respuesta && respuesta.bitcoin ? respuesta.bitcoin.usd : 0;
-  const dolarOficial = dolar ? "Dolar " + dolar.officialName + ": Compra $" + dolar.officialBuyPrice + " - Venta $" + dolar.officialSellPrice : "";
-  const dolarBlue = dolar ? "Dolar " + dolar.blueName + ": Compra $" + dolar.blueBuyPrice + " - Venta $" + dolar.blueSellPrice : "";
+  const bitcoinPrice =
+    respuesta && respuesta.bitcoin ? respuesta.bitcoin.usd : 0;
+  const dolarOficial = dolar
+    ? 'Dolar ' +
+      dolar.officialName +
+      ': Compra $' +
+      dolar.officialBuyPrice +
+      ' - Venta $' +
+      dolar.officialSellPrice
+    : '';
+  const dolarBlue = dolar
+    ? 'Dolar ' +
+      dolar.blueName +
+      ': Compra $' +
+      dolar.blueBuyPrice +
+      ' - Venta $' +
+      dolar.blueSellPrice
+    : '';
 
   return (
     <div className="card">
-      <h1>Team5</h1>
+      <FontAwesomeIcon
+        className="fw-bold fs-5 position-absolute"
+        style={{ right: '2rem', cursor: 'pointer' }}
+        icon={faSliders}
+        onClick={handleShowSettingModal}
+      ></FontAwesomeIcon>
       <p>Bitcoin: ${bitcoinPrice}</p>
       <p>{dolarOficial}</p>
       <p>{dolarBlue}</p>
+
+      {/* MODAL EDITABLE PARA CADA TEAM */}
+      <Modal show={showSettingModal} onHide={handleCloseSettingModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Configuracion</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* Aqui van la interfaz front dependiendo de la funcionalidad, en caso de que necesiten ayuda con esta parte porfavor avisarme (German).*/}
+          <form className="my-form">
+            <div className="boxInput">
+              <label htmlFor="campo1">Campo 1</label>
+              <input type="text" id="campo1" />
+            </div>
+            <div className="boxInput">
+              <label htmlFor="campo2">Campo 2</label>
+              <input type="text" id="campo2" />
+            </div>
+            <div className="boxInput">
+              <label htmlFor="campo3">Campo 3</label>
+              <input type="text" id="campo3" />
+            </div>
+            <button>Enviar</button>
+          </form>
+          {/* Este form es solo para ejemplificar, como mencione, se puede cambiar dependediendo del proyecto */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseSettingModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
